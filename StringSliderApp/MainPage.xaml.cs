f00a517b091718e78465b9c5cc8a4a811294c6c7
Mizael Sousa <aluno@MacBook-Pro-de-Aluno.local>
2019-03-12 21:31:22 -0400

    concatena e usa slider

---
+++ StringSliderApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StringSliderApp
+{
+    public partial class MainPage : ContentPage
+    {
+        void Concatenar(object sender, System.EventArgs e)
+        {
+            
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
